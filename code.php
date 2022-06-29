6877d0a4c0eb98d4decfc552c5feb0314d4fd0da
viktor_umansky <52282479+viktor-77@users.noreply.github.com>
2022-06-29 21:21:25 +0300

    first commit

---
+++ code.php
@@ -0,0 +1,5 @@
+<?php
+$str = 'ahb acb aeb aeeb adcb axeb';
+
+echo preg_replace('#a.b#', '!', $str);
+echo preg_replace('#a.b#', '!', $str);
---
+++ index.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<?php
+mb_internal_encoding('UTF-8');
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+include 'code.php';
