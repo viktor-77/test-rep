24197bb0d9a0276d15bdb99f90e2a041a82a8aff
viktor_umansky <52282479+viktor-77@users.noreply.github.com>
2022-06-30 16:50:05 +0300

    validation

--- code.php
+++ code.php
@@ -1,4 +1,10 @@
-<?php
-$str = '123444444491@1f3.fr';
-$res = preg_match('#^[0-9a-z]{10,}@[0-9a-z]{1,10}\.[a-z]{2,4}$#',$str);
-echo $res;
+//  password_validation
+//$str = '4F444f4!';
+//$res = preg_match('#^(?=.*[\!\@\#\%\^\&\*])(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\!\@\#\%\^\&\*]{8,}$#',$str);
+//echo $res;
+
+//  email_validation
+//$str = '4F444f4!';
+//$res = preg_match('#^[0-9a-z]{10,}@[0-9a-z]{1,10}\.[a-z]{2,4}$#',$str);
+//echo $res;
+
