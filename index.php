642054551cdb93e34325f2a096627a69f3c962f0
viktor_umansky <52282479+viktor-77@users.noreply.github.com>
2022-07-05 20:34:53 +0300

    1

--- index.php
+++ index.php
@@ -4,5 +4,3 @@ mb_internal_encoding('UTF-8');
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
-$url = $_SERVER['REQUEST_URI'];
-echo $url;
--- task2.php
+++ task2.php
@@ -1,3 +1,5 @@
 Выведите на index.php запрошенный URL.
 
 <?php
+$url = $_SERVER['REQUEST_URI'];
+echo $url;
