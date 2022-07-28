63e0ee6be7a9ebf4f4c2a2adf1b146cb40eae56b
Viktor <vumanskiy0664041341@gmail.com>
2022-07-28 15:47:42 +0300

    +

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ ini_set('display_errors', 'on');
 
 spl_autoload_register(function ($class) {
     $filename = str_replace('\\', '/', $class) . '.php';
+    $filename = strtolower($filename);
+
+    $arr = explode('/', $filename);
+    $arr[count($arr) - 1] = ucfirst($arr[count($arr) - 1]);
+    $filename = implode('/', $arr);
+
     require($filename);
 });
 
--- t11.php
+++ t11.php
@@ -3,6 +3,12 @@
 
 spl_autoload_register(function ($class) {
     $filename = str_replace('\\', '/', $class) . '.php';
+    $filename = strtolower($filename);
+
+    $arr = explode('/', $filename);
+    $arr[count($arr) - 1] = ucfirst($arr[count($arr) - 1]);
+    $filename = implode('/', $arr);
+
     require($filename);
 });
 
